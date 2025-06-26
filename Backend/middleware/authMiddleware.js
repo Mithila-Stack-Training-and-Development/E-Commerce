f8bfa12ffd25fdf72290e893af1e662a7ae93ccf
thakurDivya1008 <dk0065287@gmail.com>
2025-06-26 10:37:51 +0530

    User Reg, Auth and profile done.

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+//Middleware to protect routes
+
+const protect = async(req, res, next) => {
+  let token;
+  if(
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer") 
+){
+
+    try {
+    token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.user.id).select("-password"); // Exclude password
+    next();
+    }catch(error) {
+        console.error("Token verifiaction failed:", error);
+        res.status(401).json({ message: "Not  authorized, Token failed"});
+
+    }
+
+    } else{
+        res.status(401).json({message: "Not Authorized, no token provided"});
+    }
+
+  
+};
+
+module.exports = {protect};

9d77375a21ca513cf62440d853abe4742206a5a9
Akanksha6209127 <akanksha290803@gmail.com>
2025-06-30 13:51:17 +0530

    added web shopping

---
+++ my-project/src/App.jsx
@@ -0,0 +1,32 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./Components/Layout/UserLayout";
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
+
+const App = () => {
+	return (
+		<BrowserRouter
+			future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+		>
+			<Toaster position="top-right" />
+
+			<Routes>
+				<Route path="/" element={<UserLayout />}>
+					<Route index element={<Home />} />
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+					<Route path="profile" element={<Profile />} />
+					<Route path="/collections/:collection" element={<CollectionPage/>} />
+				</Route>
+				<Route>{/* Admin Layout */}</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default App;
